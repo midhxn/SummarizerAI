fb14b69bfb8e8d3727350e9af15fed0e65afd2d1
midhxn <midhunn311@gmail.com>
2023-05-10 15:25:39 +0530

    added store file

---
+++ src/services/store.js
@@ -0,0 +1,9 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { curryGetDefaultMiddleware } from "@reduxjs/toolkit/dist/getDefaultMiddleware";
+import { articleApi} from './article'
+
+export const store = configureStore({
+    reducer: {[articleApi.reducerPath]: articleApi.reducer},
+    middleware: (curryGetDefaultMiddleware) =>
+    curryGetDefaultMiddleware().concat(articleApi.middleware)
+});
